fb55f468dcf3622ad24f29ecf0bc11fde70c6f08
04stefke <jovanovics399@yahoo.com>
2024-01-21 12:27:13 +0000

    added the increment and decrement buttons and the script for them working as expected

--- project-3/scirpt.js
+++ project-3/scirpt.js
@@ -1,5 +1,21 @@
 const mainImg = document.getElementById('mainImage')
+const increment = document.getElementById('incrementBtn')
+const decrement = document.getElementById('decrementBtn')
+let quantity = document.getElementById('quantity')
 
+let count = 0
+
+increment.addEventListener('click', () => {
+    count++
+    quantity.value = count
+})
+
+decrement.addEventListener('click', () => {
+    if(count > 0){
+        count--
+        quantity.value = count
+    }
+})
 
 
 function changeImage(imagePath) {
