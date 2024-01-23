d55f344a3bda7329709d01a8c31cec4fcff83f3d
04stefke <jovanovics399@yahoo.com>
2024-01-23 19:55:47 +0000

    fixed the lightbox effect script

--- project-3/scirpt.js
+++ project-3/scirpt.js
@@ -39,28 +39,76 @@ let count = 1
         }
     })
 
-    thumbnail.forEach(thumb => thumb.addEventListener('click', makeActive))
+thumbnail.forEach(thumb => thumb.addEventListener('click', makeActive))
 
-    function makeActive(event){
-        thumbnail.forEach(thumb => {
-            thumb.classList.remove('active')
-            event.currentTarget.classList.add('active')
-        })
-    }
+function makeActive(event){
+    thumbnail.forEach(thumb => {
+        thumb.classList.remove('active')
+        event.currentTarget.classList.add('active')
+    })
+}
 
-    function changeImage(imagePath) {
-        mainImg.firstElementChild.src = imagePath
-        
-    }
+function changeImage(imagePath) {
+    mainImg.firstElementChild.src = imagePath
+    
+}
 
 function closeLightbox(){
     document.getElementById('lightboxModal').style.display = 'none'
 }
 
-var slideIndex = 1
-
 function changeImg(imgSrc){
     document.querySelector('#mainImage img').src = imgSrc
+}
+
+function openLightbox(selectedImageSrc){
+    var lightbox = document.getElementById('lightboxModal')
+    var lightboxMainImage = document.querySelector('.main-image img')
+    var thumbnails = document.querySelectorAll('.thumbnail-container img')
+    
+    lightboxMainImage.src = selectedImageSrc
+
+    thumbnails.forEach(function(thumbnail) {
+        thumbnail.classList.remove('active');
+        if (thumbnail.src.includes(selectedImageSrc)) {
+          thumbnail.classList.add('active');
+        }
+    })
+
+    lightbox.style.display = 'block'
+}
+
+function changeLightboxImage(imgSrc){
+    var lightboxMainImage = document.querySelector('.lightbox .main-image img');
+    lightboxMainImage.src = imgSrc;
+  
+    
+    var thumbnails = document.querySelectorAll('.thumbnail-container img');
+    thumbnails.forEach(function(thumb) {
+      thumb.classList.remove('active');
+      if (thumb.src.includes(imgSrc)) {
+        thumb.classList.add('active');
+      }
+    });
+}
+
+const images = document.querySelector('.defPic')
+if (images) {
+    images.addEventListener('click', function() {
+        openLightbox(images.src);
+    });
+}
+var slideIndex = 1
+
+function showSlides(n) {
+    var i
+    var slides = document.getElementsByClassName('lightbox-slide')
+    if(n > slides.length) {slideIndex = 1}
+    if(n < 1) {slideIndex = slides.length}
+    for(i = 0; i < slides.length; i++){
+        slides[i].style.display = 'none'
+    }
+    slides[slideIndex - 1].style.display = 'block'
 }
 
 function setCurrentSlide(n) {
@@ -73,58 +121,4 @@ function moveSlide(n){
 
 function currentSlide(n) {
     showSlides(slideIndex = n)
-}
-
-function showSlides(n) {
-    var i
-    var slides = document.getElementsByClassName('lightbox-slide')
-    if(n > slides.length) {slideIndex = 1}
-    if(n < 1) {slideIndex = slides.length}
-    for(i = 0; i < slides.length; i++){
-        slides[1].style.display = 'none'
-    }
-    slides[slideIndex - 1].style.display = 'block'
-}
-
-const images = document.querySelector('.defPic')
-images.forEach(image => {
-    image.addEventListener('click', e => {
-        lightbox.classList.add('active')
-        const img = document.createElement('img')
-        img.src = image.src
-        while(lightbox.firstChild){
-            lightbox.removeChild(lightbox.firstChild)
-        }
-        lightbox.appendChild(img)
-    })
-})
-
-function openLightbox(selectedImageSrc){
-    var lightbox = document.getElementById('lightboxModal')
-    var lightboxMainImage = document.querySelector('.lightbox .main-image img')
-    var thumbnails = document.querySelectorAll('.lightbox .thumbnail-container img')
-    
-    lightboxMainImage.src = selectedImageSrc
-
-    thumbnails.forEach(function(thumbnail) {
-        if(thumbnail.src === selectedImageSrc) {
-            thumbnail.classList.add('active')
-        } else {
-            thumbnail.classList.remove('active')
-        }
-    })
-
-    lightbox.style.display = 'block'
-}
-
-function changeLightboxImage(imgSrc){
-    document.querySelector('.lightbox .main-image img').src = imgSrc
-
-    var thumbnails = document.querySelectorAll('.lightbox .thumbnail-container img')
-    thumbnails.forEach(function(thumb) {
-        thumb.classList.remove('active')
-        if(thumb.src === imgSrc) {
-            thumb.classList.add('active')
-        }
-    })
 }
